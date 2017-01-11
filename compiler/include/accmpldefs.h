96b91388b360aa0fb9b53bb39194e05a61709b6d
achaari <achaari@users.noreply.github.com>
2017-01-11 18:47:34 +0100

    Update accmpldefs.h

--- compiler/include/accmpldefs.h
+++ compiler/include/accmpldefs.h
@@ -29,17 +29,17 @@ typedef enum {
 typedef void *p_accmpl_proc_;
 typedef int(*__exec)(p_accmpl_ cmplhndp);
 
-AC_EXTERN p_accmpl_proc_ __ac_init_proc(p_accmpl_ cmplhndp, const char *procname, e_proc_type type);
+AC_EXTERN int __ac_init_proc(p_accmpl_ cmplhndp, const char *procname);
 
 AC_EXTERN int __ac_compl_exec_mainproc(p_accmpl_ cmplhndp, __exec prcfctp);
 AC_EXTERN int __ac_process_step(p_accmpl_ cmplhndp, int checkstepb, e_ac_step_ step, ...);
-AC_EXTERN int __ac_end_proc(p_accmpl_ cmplhndp, p_accmpl_proc_ *procpp);
-AC_EXTERN int __ac_stop_proc(p_accmpl_ cmplhndp, p_accmpl_proc_ *procpp);
+AC_EXTERN int __ac_end_proc(p_accmpl_ cmplhndp);
+AC_EXTERN int __ac_stop_proc(p_accmpl_ cmplhndp);
 AC_EXTERN int __ac_pocess_next(p_accmpl_ cmplhndp);
 
 #define __ac_proc_step(mand, ...)  __ac_process_step(cmplhndp, mand, __VA_ARGS__, AC_STEP_END_EXEC)
 #define __ac_check_step(stat, ...) __ac_proc_step(FALSE, stat, __VA_ARGS__)
-#define __ac_exec_step(stat, ...)  if(! __ac_proc_step(TRUE, stat, __VA_ARGS__)) { return(__ac_stop_proc(cmplhndp, &procp)); }
+#define __ac_exec_step(stat, ...)  if(! __ac_proc_step(TRUE, stat, __VA_ARGS__)) { return(__ac_stop_proc(cmplhndp)); }
 #define __ac_try_step(stat, ...)   __ac_proc_step(FALSE, stat, __VA_ARGS__);
 
 #define AND                    ,AC_STEP_APPEND_AND, 
