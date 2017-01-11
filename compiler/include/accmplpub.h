a8363c251672119d833be6c18ee7c039e89dab23
achaari <achaari@users.noreply.github.com>
2017-01-11 18:47:09 +0100

    Update accmplpub.h

--- compiler/include/accmplpub.h
+++ compiler/include/accmplpub.h
@@ -14,7 +14,7 @@ AC_EXTERN p_accmpl_ __ac_new_compiler();
 
 AC_EXTERN int __ac_compl_set_keywords(p_accmpl_ cmplhndp, const char **keywords, int count);
 AC_EXTERN int __ac_compl_set_tokens(p_accmpl_ cmplhndp, const char **tokens, int count);
-AC_EXTERN int __ac_compl_exec(p_accmpl_ cmplhndp, int *cmpldatap, int count);
+AC_EXTERN int __ac_compl_exec(p_accmpl_ cmplhndp, const int *cmpldatap, int count);
 AC_EXTERN int __ac_exit_compiler(p_accmpl_ *cmplhndp);
 
 #endif
